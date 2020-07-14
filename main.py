58b496fd376ca9f9162cc39a65af804ddc0eb85e
Kayla Lemessy <kaylalemessy15@gmail.com>
2020-07-14 18:01:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("My name is Kayla Lemessy")
+
+print("My favorite artist is BTS")
+
+print(" My favorite sport is soccer")
+
+print("My place to go to vacaction is Japan")
+
+print("July 14, 2020")
+
